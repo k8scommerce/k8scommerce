f7610b6fd1ad9aa593e9536fb0be7f201d8e139a
Alma Tuck <alma.tuck@gmail.com>
2022-01-01 18:25:33 -0700

    hotfix shipping

--- services/rpc/shipping/internal/config/config.go
+++ services/rpc/shipping/internal/config/config.go
@@ -4,12 +4,4 @@ import "github.com/tal-tech/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
-	Postgres PostgresConfig
-}
-
-type PostgresConfig struct {
-	Connection                   string
-	MaxOpenConnections           int
-	MaxIdleConnections           int
-	MaxConnectionLifetimeMinutes int
 }
