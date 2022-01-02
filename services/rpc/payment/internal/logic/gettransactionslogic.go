0e01f67bd7e0dcf4128251e2e39eba745e255596
Alma Tuck <alma.tuck@gmail.com>
2022-01-01 18:18:59 -0700

    hotfix payment

--- services/rpc/payment/internal/logic/gettransactionslogic.go
+++ services/rpc/payment/internal/logic/gettransactionslogic.go
@@ -2,40 +2,30 @@ package logic
 
 import (
 	"context"
-	"sync"
 
-	"github.com/k8scommerce/k8scommerce/services/rpc/payment/internal/svc"
-	"github.com/k8scommerce/k8scommerce/services/rpc/payment/pb/payment"
-
+	"github.com/k8scommerce/k8scommerce/services/rpc/client/internal/svc"
+	"github.com/k8scommerce/k8scommerce/services/rpc/client/pb/payment"
 	"github.com/localrivet/galaxycache"
+
 	"github.com/tal-tech/go-zero/core/logx"
 )
-
-type galaxyGetTransactionsLogicHelper struct {
-	once   *sync.Once
-	galaxy *galaxycache.Galaxy
-}
-
-var entryGetTransactionsLogic *galaxyGetTransactionsLogicHelper
 
 type GetTransactionsLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 	logx.Logger
-	universe *galaxycache.Universe
-	mu       sync.Mutex
 }
 
 func NewGetTransactionsLogic(ctx context.Context, svcCtx *svc.ServiceContext, universe *galaxycache.Universe) *GetTransactionsLogic {
 	return &GetTransactionsLogic{
-		ctx:      ctx,
-		svcCtx:   svcCtx,
-		Logger:   logx.WithContext(ctx),
-		universe: universe,
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
 	}
 }
 
 func (l *GetTransactionsLogic) GetTransactions(in *payment.ProcessPaymentRequest) (*payment.ProcessPaymentResponse, error) {
 	res := &payment.ProcessPaymentResponse{}
 	return res, nil
+
 }
