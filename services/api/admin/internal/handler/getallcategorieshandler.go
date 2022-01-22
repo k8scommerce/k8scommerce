181f4f94e06594afced137d19f0e59cc7ed19042
Alma Tuck <alma.tuck@gmail.com>
2022-01-22 12:16:11 -0700

    documentation update

--- services/api/admin/internal/handler/getallcategorieshandler.go
+++ services/api/admin/internal/handler/getallcategorieshandler.go
@@ -6,12 +6,19 @@ import (
 	"github.com/tal-tech/go-zero/rest/httpx"
 	"k8scommerce/services/api/admin/internal/logic"
 	"k8scommerce/services/api/admin/internal/svc"
+	"k8scommerce/services/api/admin/internal/types"
 )
 
 func getAllCategoriesHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.GetAllCategoriesRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
 		l := logic.NewGetAllCategoriesLogic(r.Context(), svcCtx)
-		resp, err := l.GetAllCategories()
+		resp, err := l.GetAllCategories(req)
 		if err != nil {
 			httpx.Error(w, err)
 		} else {
